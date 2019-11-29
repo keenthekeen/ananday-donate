339e88e7c5b4150e22edf2111bfed0b2ba099b86
Sarun Intaralawan <sarunint@docchula.com>
2019-11-30 04:38:24 +0700

    fix: use correct import for `firebase/app`

--- src/app/admin/edit/edit.component.ts
+++ src/app/admin/edit/edit.component.ts
@@ -5,7 +5,7 @@ import { Observable, combineLatest } from 'rxjs';
 import { map, switchMap, first } from 'rxjs/operators';
 import { FirebaseApp } from '@angular/fire';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { firebase } from '@firebase/app';
+import { firebase } from 'firebase/app';
 import 'firebase/database';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FormControl, FormBuilder, Validators } from '@angular/forms';
