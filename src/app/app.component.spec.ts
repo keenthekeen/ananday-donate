fd59743c1d3cf54a2cbb850084e285b7f0f6c5d6
Siwat Techavoranant <keen@keendev.net>
2020-04-09 01:07:33 +0700

    feat: add basic unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      imports: [
+        RouterTestingModule,
+        FontAwesomeModule
+      ],
+      declarations: [
+        AppComponent
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,17 +20,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'and'`, async(() => {
+  it(`should have as collapse 'true'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('and');
+    expect(app.collapse).toEqual(true);
   }));
-  it('should render title in a h1 tag', async(() => {
+  it(`should render footer with text 'ANAN DAY'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to and!'
-    );
+    expect(compiled.querySelector('footer a').textContent).toContain('ANAN DAY');
   }));
 });
